f0e19d0aba11e3a8b7917c76793c2dc01c00623e
GauravThantry <gaurav.thantry@gmail.com>
2018-04-01 14:03:49 +0530

    Added click count to javascript

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
 $(".deck-content").flip({
   trigger: 'manual'
  });
- 
+ var i=0;
+ var clickCount=0;
+
  $(".flip").click(function(){
+   if(clickCount<2)
+   {
 	 $(this).closest(".deck-content").flip(true);
+   i++;
+   clickCount++;
+   }
  });
- 
- $(".unflip).click(function(){
+
+ $(".unflip").click(function(){
 	 $(this).closest(".deck-content").flip(false);
  });
 });

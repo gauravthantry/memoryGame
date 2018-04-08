514752a79db12116a35a595e60154d3860bc2eb2
GauravThantry <gaurav.thantry@gmail.com>
2018-04-08 16:27:15 +0530

    Added a new design

--- js/app.js
+++ js/app.js
@@ -1,67 +1,138 @@
-$(document).ready(function() {
-    var anchor = '<i class="fa fa-anchor fa-prop" value="anchor"></i>';
-    var assist = '<i class="fa fa-assistive-listening-systems fa-prop" value="assist"></i>';
-    var bicycle = '<i class="fa fa-bicycle fa-prop" value="bicycle"></i>';
-    var signLang = '<i class="fa fa-american-sign-language-interpreting fa-prop" value="sign"></i>';
-    var bitcoin = '<i class="fa fa-bitcoin fa-prop" value="bitcoin"></i>';
-    var angellist = '<i class="fa fa-angellist fa-prop" value="angellist"></i>';
-    var bug = '<i class="fa fa-bug fa-prop" value="bug"></i>';
-    var cubes = '<i class="fa fa-cubes fa-prop" value="cubes"></i>';
-     var app = {
-       cards: [anchor,assist,bicycle,signLang,bitcoin,angellist,bug,cubes],
-       init: function() {
-         app.shuffle();
-       },
-       shuffle: function() {
-         var random = 0;
-         var temp = 0;
-         for (i = 1; i < app.cards.length; i++) {
-           random = app.cards[Math.round(Math.random() * i)];
-           temp = app.cards[i];
-           app.cards[i] = app.cards[random];
-           app.cards[random] = temp;
-         }
-         app.assignCards();
-         console.log('Shuffled Card Array: ' + app.cards);
-       },
-       assignCards: function() {
-         $('.card').each(function(index) {
-           $(this).append(app.cards[index]);
-         });
-         app.clickHandlers();
-       },
-       clickHandlers: function() {
-         $('.card').on('click', function() {
-           $(this).html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
-           app.checkMatch();
-         });
-       },
-       checkMatch: function() {
-         if ($('.selected').length === 2) {
-           if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
-             $('.selected').each(function() {
-               $(this).animate({
-                 opacity: 0
-               }).removeClass('unmatched');
-             });
-             $('.selected').each(function() {
-               $(this).removeClass('selected');
-             });
-             app.checkWin();
-           } else {
-             setTimeout(function() {
-               $('.selected').each(function() {
-                 $(this).html('').removeClass('selected');
-               });
-             }, 1000);
-           }
-         }
-       },
-       checkWin: function() {
-         if ($('.unmatched').length === 0) {
-           $('.container').html('<h1>You Won!</h1>');
-         }
-       }
-     };
-     app.init();
-   });
+var symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
+		opened = [],
+		match = 0,
+		moves = 0,
+		$deck = $('.deck'),
+		$scorePanel = $('#score-panel'),
+		$moveNum = $scorePanel.find('.moves'),
+		$ratingStars = $scorePanel.find('i'),
+		$restart = $scorePanel.find('.restart'),
+		delay = 800,
+		gameCardsQTY = symbols.length / 2,
+		rank3stars = gameCardsQTY + 2,
+		rank2stars = gameCardsQTY + 6,
+		rank1stars = gameCardsQTY + 10;
+
+// Shuffle function From http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Initial Game
+function initGame() {
+	var cards = shuffle(symbols);
+  $deck.empty();
+  match = 0;
+  moves = 0;
+  $moveNum.html(moves);
+  $ratingStars.removeClass('fa-star-o').addClass('fa-star');
+	for (var i = 0; i < cards.length; i++) {
+		$deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
+	}
+};
+
+// Set Rating and final Score
+function setRating(moves) {
+	var rating = 3;
+	if (moves > rank3stars && moves < rank2stars) {
+		$ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+		rating = 2;
+	} else if (moves > rank2stars && moves < rank1stars) {
+		$ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+		rating = 1;
+	} else if (moves > rank1stars) {
+		$ratingStars.eq(0).removeClass('fa-star').addClass('fa-star-o');
+		rating = 0;
+	}
+	return { score: rating };
+};
+
+// End Game
+function endGame(moves, score) {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Congratulations! You Won!',
+		text: 'With ' + moves + ' Moves and ' + score + ' Stars.\nBoom Shaka Lak!',
+		type: 'success',
+		confirmButtonColor: '#9BCB3C',
+		confirmButtonText: 'Play again!'
+	}).then(function(isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
+}
+
+// Restart Game
+$restart.on('click', function() {
+  swal({
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    title: 'Are you sure?',
+    text: "Your progress will be Lost!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#9BCB3C',
+    cancelButtonColor: '#EE0E51',
+    confirmButtonText: 'Yes, Restart Game!'
+  }).then(function(isConfirm) {
+    if (isConfirm) {
+      initGame();
+    }
+  })
+});
+
+// Card flip
+$deck.on('click', '.card:not(".match, .open")', function() {
+	if($('.show').length > 1) { return true; }
+
+	var $this = $(this),
+			card = $this.context.innerHTML;
+  $this.addClass('open show');
+	opened.push(card);
+
+	// Compare with opened card
+  if (opened.length > 1) {
+    if (card === opened[0]) {
+      $deck.find('.open').addClass('match animated infinite rubberBand');
+      setTimeout(function() {
+        $deck.find('.match').removeClass('open show animated infinite rubberBand');
+      }, delay);
+      match++;
+    } else {
+      $deck.find('.open').addClass('notmatch animated infinite wobble');
+			setTimeout(function() {
+				$deck.find('.open').removeClass('animated infinite wobble');
+			}, delay / 1.5);
+      setTimeout(function() {
+        $deck.find('.open').removeClass('open show notmatch animated infinite wobble');
+      }, delay);
+    }
+    opened = [];
+		moves++;
+		setRating(moves);
+		$moveNum.html(moves);
+  }
+
+	// End Game if match all cards
+	if (gameCardsQTY === match) {
+		setRating(moves);
+		var score = setRating(moves).score;
+		setTimeout(function() {
+			endGame(moves, score);
+		}, 500);
+  }
+});
+
+initGame();

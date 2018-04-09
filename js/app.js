86676229081ed85593d7031f2f903941e0dccd78
GauravThantry <gaurav.thantry@gmail.com>
2018-04-09 22:33:34 +0530

    Added if loop to restrict clicks to two

--- js/app.js
+++ js/app.js
@@ -11,13 +11,22 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 $(document).ready(function(){
+  var click=1;
     $(".card").click(function(){
+      if(click===1)
+      {
       $(this).addClass("open show");
-    })
+      click++;
+      }
+      if(click===2)
+      {
+        $(this).addClass("open show");
+      }
+      click=1;
   });
   function shuffle(array) {
       var currentIndex = array.length, temporaryValue, randomIndex;
-  
+
       while (currentIndex !== 0) {
           randomIndex = Math.floor(Math.random() * currentIndex);
           currentIndex -= 1;
@@ -25,11 +34,11 @@ $(document).ready(function(){
           array[currentIndex] = array[randomIndex];
           array[randomIndex] = temporaryValue;
       }
-  
+
       return array;
   }
-  
-  
+
+
   /*
    * set up the event listener for a card. If a card is clicked:
    *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -40,4 +49,3 @@ $(document).ready(function(){
    *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
    *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
    */
-  

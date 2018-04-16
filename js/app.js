9fc8071e70a121016c9f37b141eb5b4afe7550a1
GauravThantry <gaurav.thantry@gmail.com>
2018-04-16 13:15:13 +0530

    Added callback function to fix a bug Bug: when three cards a clicked before the unflipping of the firsst two unmatched element completes, the lates two cards were getting unflipped leaving the firsst card flipped. Added a callback function that waits for the first two cards to be unflipped before the third card is flipped.

--- js/app.js
+++ js/app.js
@@ -1,72 +1,72 @@
 $(document).ready(function() {
-    var click = 1,totalClicks = 0, className1 = '',className2 = '',firstClick='',secondClick='',match=0;
+    var click = 1,
+        totalClicks = 0,
+        className1 = '',
+        className2 = '',
+        firstClick = '',
+        secondClick = '',
+        match = 0;
     shuffle();
     $(".moves").html(totalClicks);
-    $(".card").on('click',function() {
-      if($(this).attr('disabled')=="disabled"){
-        return false;
-      }
-  else{
-        if (!$(this).hasClass("open")) {
-            totalClicks++;
-            $(".moves").html(totalClicks);
-            if (click === 1) {
-                $(this).addClass("open");
-                $(this).addClass("show");
-              /*  $(this).attr('id', 'card1'); */
-                className1 = $(this).children().attr('class');
-                firstClick=$(this);
-            } else if (click === 2) {
-                $(this).addClass("open show");
-                className2 = $(this).children().attr('class');
-                secondClick=$(this);
-                if(className1===className2)
-                {
-                  match++;
-                  secondClick.attr("disabled","disabled");   /* Used to remove click feature from matched cards */
-                  firstClick.attr("disabled","disabled");
-                  firstClick.addClass("match");
-                  secondClick.addClass("match");
-                }
-                if(match===8)
-                {
-                $("#overlay").css("display","block");
-                if(totalClicks<=20&&totalClicks>=16)
-                {
-                $(".text").hide().html('Fantabulous!!You are too quick!!!!').fadeIn('slow');
-                }
-                if(totalClicks<=25&&totalClicks>20)
-                {
-                  $(".text").hide().html('Excellent!!You\'ve got the license to make it rocket speed').fadeIn('slow');
-                }
-                if(totalClicks>25)
-                {
-                  $(".text").hide().html('Good Warm up! Try getting faster').fadeIn('slow');
-                }
+    $(".card").on('click', function() {
+        if ($(this).attr('disabled') == "disabled") {
+            return false;
+        } else {
+            if (!$(this).hasClass("open")) {
+                totalClicks++;
+                $(".moves").html(totalClicks);
+                if (click === 1) {
+                    $(this).addClass("open");
+                    $(this).addClass("show");
+                    /*  $(this).attr('id', 'card1'); */
+                    className1 = $(this).children().attr('class');
+                    firstClick = $(this);
+                } else if (click === 2) {
+                    $('ul.deck *').attr("disabled", "disabled");
+                    $(this).addClass("open show");
+                    className2 = $(this).children().attr('class');
+                    secondClick = $(this);
+                    if (className1 === className2) {
+                        match++;
+                        firstClick.addClass("match");
+                        secondClick.addClass("match");
+                    }
+                    unflipAndRemoveAttr();
+                    if (match === 8) {
+                        $("#overlay").css("display", "block").hide().fadeIn(500);
+                        if (totalClicks <= 20 && totalClicks >= 16) {
+                            $(".text").hide().html('Fantabulous!!You are too quick!!!!').fadeIn(1000);
+                        }
+                        if (totalClicks <= 25 && totalClicks > 20) {
+                            $(".text").hide().html('Excellent!!You\'ve got the license to make it rocket speed').fadeIn(1000);
+                        }
+                        if (totalClicks > 25) {
+                            $(".text").hide().html('Good Warm up! Try getting faster').fadeIn(1000);
+                        }
+
+                    }
+                    $(".playagain").css("display", "block").fadeIn(1500);
 
                 }
-                unflip();
-            }
-            if (click === 1) {
-                click++;
+                if (click === 1) {
+                    click++;
+                } else {
+                    click = 1;
+                }
             } else {
                 click = 1;
+                $(this).removeClass("open");
+                $(this).removeClass("show");
             }
-        }
-        else{
-          click=1;
-          $(this).removeClass("open");
-          $(this).removeClass("show");
-        }
 
-}
+        }
     });
 
     $(".restart").click(function() {
-      $(this).children().addClass('refresh').delay(200).queue(function(next){
-        $(this).removeClass('refresh');
-        next();
-      });
+        $(this).children().addClass('refresh').delay(200).queue(function(next) {
+            $(this).removeClass('refresh');
+            next();
+        });
         totalClicks = 0;
         $(".moves").html(totalClicks);
         $("ul.deck>li").removeClass("open");
@@ -78,26 +78,42 @@ $(document).ready(function() {
         }
     });
 
-    $(".restart-overlay").click(function(){
-      className1='';
-      className2='';
-      match=0;
-      $("#overlay").css("display","none");
-      totalClicks = 0;
-      $(".moves").html(totalClicks);
-    $("ul.deck>li").removeClass("open show match");
-    var deck = document.querySelector(".deck");
-    for (var i = deck.children.length; i >= 0; i--) {
-        deck.appendChild(deck.children[Math.random() * i | 0]);
-    $('ul.deck *').removeAttr('disabled'); /*Used to re-enable the cards for cliking after refreshing the deck */
-  }
+    $(".restart-overlay").click(function() {
+        className1 = '';
+        className2 = '';
+        match = 0;
+        $("#overlay").css("display", "none");
+        totalClicks = 0;
+        $(".moves").html(totalClicks);
+        $("ul.deck>li").removeClass("open show match");
+        var deck = document.querySelector(".deck");
+        for (var i = deck.children.length; i >= 0; i--) {
+            deck.appendChild(deck.children[Math.random() * i | 0]);
+            $('ul.deck *').removeAttr('disabled'); /*Used to re-enable the cards for cliking after refreshing the deck */
+        }
 
     });
-    function unflip() {
-        if (className1 !== className2) {
-            setTimeout(removeClasses, 1000);
-        }
+
+
+    function unflip(callback) {
+        setTimeout(function() {
+            if (className1 !== className2) {
+                removeClasses();
+                if (typeof callback == 'function')
+                    callback();
+            }
+        }, 1000);
     }
+
+
+    function removeAttribute() {
+        $("li:not(.match)").removeAttr('disabled');
+    }
+
+    function unflipAndRemoveAttr() {
+        unflip(removeAttribute);
+    }
+
     function removeClasses() {
         firstClick.removeClass("open");
         firstClick.removeClass("show");
@@ -105,10 +121,10 @@ $(document).ready(function() {
         secondClick.removeClass("show");
     }
 
-    function shuffle(){
-    var deck = document.querySelector(".deck");
-    for (var i = deck.children.length; i >= 0; i--) {
-        deck.appendChild(deck.children[Math.random() * i | 0]);
+    function shuffle() {
+        var deck = document.querySelector(".deck");
+        for (var i = deck.children.length; i >= 0; i--) {
+            deck.appendChild(deck.children[Math.random() * i | 0]);
+        }
     }
-  }
 });
